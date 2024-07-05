ef9b0842a39871ac3c01410987943d79ec915f27
Vincentius Oktara <108650831+Vincent1646@users.noreply.github.com>
2024-07-05 16:27:07 +0700

    finish procedure

--- Procedure & Function/Procedure.sql
+++ Procedure & Function/Procedure.sql
@@ -23,11 +23,30 @@ based on the parameter value(s) that is passed.
    for subsequent executions.
 */
 --============================================================================================================
-
 USE TamasyaCafe
 SELECT *
 FROM customer
 
+
+-- #1 Searching a String
+-- =========================
+/*Giving result to all string that includes a string like we input with wildcard*/
+GO
+CREATE OR ALTER PROCEDURE searchName
+(@customerName VARCHAR(MAX) = NULL)
+AS
+BEGIN
+	SELECT CustomerName AS [Name], CustomerPoint AS [Point]
+	FROM customer
+	WHERE CustomerName LIKE '%' + @customerName + '%'
+END
+GO
+--
+EXEC searchName[Graisu Citasya]
+--
+--DROP PROCEDURE [proc_name]
+--
+/*Giving result to a string that have the exact matches to our string [CASE SENSITIVE]*/
 GO
 CREATE OR ALTER PROCEDURE searchCustomer
 (@customerName VARCHAR(MAX) = NULL)
@@ -38,5 +57,46 @@ BEGIN
 	WHERE CustomerName = @customerName
 END
 GO
+--
+EXEC searchCustomer [Joceline Aoki]
+--
 
-EXEC searchCustomer [Graisu Citasya]
+-- #2 DML with Procedure
+-- =========================
+GO
+CREATE OR ALTER PROCEDURE inputCustomer
+(@customerId VARCHAR(MAX), @customerName VARCHAR(MAX), @customerPoint INT)
+AS BEGIN
+	INSERT INTO customer
+	VALUES (@customerId, @customerName, @customerPoint)
+END
+--
+EXEC inputCustomer [DX488], [Drix Kohar], 628
+SELECT *
+FROM customer
+-- 
+GO
+CREATE OR ALTER PROCEDURE removeCustomer
+(@customerId VARCHAR(MAX))
+AS BEGIN
+	DELETE FROM customer
+	WHERE CustomerId = @customerId 
+END
+--
+EXEC removeCustomer [DX488]
+SELECT *
+FROM customer
+--
+GO
+CREATE OR ALTER PROCEDURE updateCustomer
+(@customerID VARCHAR(MAX), @customerName VARCHAR(MAX))
+AS BEGIN
+	UPDATE customer
+	SET CustomerName = @customerName
+	WHERE CustomerId = @customerID
+END
+--
+EXEC updateCustomer [YC694], [YouVan]
+SELECT *
+FROM customer
+--
